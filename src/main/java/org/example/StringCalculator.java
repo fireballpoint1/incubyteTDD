79d728266641f3bb2f6d9759f670b58913ca7cc5
fireballpoint1 <fireballpoint1>
2024-12-19 01:41:49 +0530

    develop: added solution to manage a 2 numbers in input

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,7 +3,13 @@ package org.example;
 public class StringCalculator {
     public static int add(String numbers) {
         if(numbers.isEmpty()) return 0;
-        else
-            return Integer.parseInt(numbers);
+        else{
+            String[] numbersArray = numbers.split(",");
+            if(numbersArray.length > 1){
+                return Integer.parseInt(numbersArray[0]) + Integer.parseInt(numbersArray[1]);
+            } else {
+                return Integer.parseInt(numbersArray[0]);
+            }
+        }
     }
 }
