2c1525dea483fd671380fe3423d81e53f99ec315
fireballpoint1 <fireballpoint1>
2024-12-19 01:57:56 +0530

    develop: for strings starting with //, implemented logic to use custom delimiter instead of \n and ,

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,7 +2,14 @@ package org.example;
 
 public class StringCalculator {
     public static int add(String numbers) {
-        String[] numbersArray = numbers.split(("[\n,]"));
+        String[] numbersArray = new String[0];
+        if(numbers.startsWith("//")) {
+            String[] parts = numbers.split("\n", 2);
+            String delimiter = parts[0].substring(2);
+            numbersArray = parts[1].split(delimiter);
+        } else {
+            numbersArray = numbers.split(("[\n,]"));
+        }
         int sum=0;
         for (String number : numbersArray) {
             if (!number.trim().isEmpty()) {
