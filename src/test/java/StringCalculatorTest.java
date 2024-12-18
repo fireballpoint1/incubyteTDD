cf4afccd3842d7c51d656b866d370e9535a83784
fireballpoint1 <fireballpoint1>
2024-12-19 01:30:27 +0530

    test: added test for a single number

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,13 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.example.StringCalculator.add;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringCalculatorTest {
     @Test
     public void givenEmptyStringReturnZero() {
-
-
-        assertEquals(add(),0);
+        assertEquals(add(""),0);
     }
 }
